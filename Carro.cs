8c411a544567a28c07eff282d83f5452b3e0662f
JhoaoAle <33556671+JhoaoAle@users.noreply.github.com>
2019-07-07 12:45:03 -0500

    Creación clase Carro para facilitar inserción

---
+++ Carro.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManejoInventariosBD
+{
+    public class Carro
+    {
+        public Carro() { }
+
+        public static string BomDel { get; set; }
+        public static string Persiana { get; set; }
+        public static string FarIzq { get; set; }
+        public static string ParDel { get; set; }
+        public static string EspRetIzq { get; set; }
+        public static string GFDelIzq { get; set; }
+        public static string PDelIzq { get; set; }
+        public static string VPDelIzq { get; set; }
+        public static string VPTraIzq{ get; set; }
+        public static string GFTraIzq { get; set; }
+        public static string ParTra { get; set; }
+        public static string BomTra { get; set; }
+        public static string StopIzq { get; set; }
+        public static string PTraDer { get; set; }
+        public static string GFTraDer { get; set; }
+        public static string PDelDer { get; set; }
+        public static string VPDelDer { get; set; }
+        public static string VPTraDer { get; set; }
+        public static string GFDelDer { get; set; }
+        public static string EspRetDer { get; set; }
+        public static string FarDer { get; set; }
+        public static string DirDelDer { get; set; }
+        public static string ExpDel { get; set; }
+        public static string DirDelIzq { get; set; }
+        public static string CojDel { get; set; }
+        public static string CojTra { get; set; }
+        public static string EspInt { get; set; }
+        public static string RadPas { get; set; }
+        public static string Par { get; set; }
+        public static string AntRadPas { get; set; }
+        public static string Encendedor { get; set; }
+        public static string RadTel { get; set; }
+        public static string AntRadTel { get; set; }
+        public static string LlaRes { get; set; }
+        public static string Herramientas { get; set; }
+        public static string Bateria { get; set; }
+        public static string Limpiabrisas { get; set; }
+        public static string Otros { get; set; }
+        public static string Marca { get; set; }
+        public static string PlacaNum { get; set; }
+        public static string Color { get; set; }
+        public static Boolean debegrua { get; set; }
+        public static Boolean dejallaves { get; set; }
+        public static int NumPar { get; set; }
+    }
+}
