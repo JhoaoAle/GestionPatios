64e154d732e7ff2578d197bec8e2fee859a3eb6f
JhoaoAle <33556671+JhoaoAle@users.noreply.github.com>
2019-07-07 21:01:20 -0500

    Completa la clase moto, sigue realizar las asignaciones en los formularios y de ahí la inserción

--- Moto.cs
+++ Moto.cs
@@ -14,49 +14,433 @@ namespace ManejoInventariosBD
 
         public Moto() { }
 
-        public static string Farola { get; set; }
-        public static string Exploradora { get; set; }
-        public static string Carenaje { get; set; }
-        public static string Pito { get; set; }
-        public static string GBDel { get; set; }
-        public static string FrenoDisco { get; set; }
-        public static string Tablero { get; set; }
-        public static string DirDelIzq { get; set; }
-        public static string EspIzq { get; set; }
-        public static string ManDirIzq { get; set; }
-        public static string ManIzq { get; set; }
-        public static string PrManIzq { get; set; }
-        public static string DirDelDer { get; set; }
-        public static string EspDer { get; set; }
-        public static string ManDirDer { get; set; }
-        public static string ManFreDer { get; set; }
-        public static string PrManFreDer { get; set; }
-        public static string TanCom { get; set; }
-        public static string TTanCom { get; set; }
-        public static string Barbero { get; set; }
-        public static string Pata { get; set; }
-        public static string Gato { get; set; }
-        public static string TapLatIzq { get; set; }
-        public static string GCad { get; set; }
-        public static string Sillin { get; set; }
-        public static string Parrilla { get; set; }
-        public static string GBarDel { get; set; }
-        public static string DirTraIzq { get; set; }
-        public static string Amortiguadores { get; set; }
-        public static string Exosto { get; set; }
-        public static string Placa { get; set; }
-        public static string DirTraDer { get; set; }
-        public static string TapLatDer { get; set; }
-        public static string Bateria { get; set; }
-        public static string Tacos { get; set; }
-        public static string PalFrePie { get; set; }
-        public static string Cran { get; set; }
-        public static string Otros { get; set; }
-        public static string Marca { get; set; }
-        public static string PlacaNum { get; set; }
-        public static string Color { get; set; }
-        public static Boolean debegrua { get; set; }
-        public static Boolean dejallaves { get; set; }
+        public static string Farola;
+        public void SetFarola(string x)
+        {
+            Farola= x;
+        }
+
+        public string GetFarola()
+        {
+            return Farola;
+        }
+        public static string Exploradora;
+        public void SetExploradora(string x)
+        {
+            Exploradora = x;
+        }
+
+        public string GetExploradora()
+        {
+            return Exploradora;
+        }
+        public static string Carenaje;
+        public void SetCarenaje(string x)
+        {
+            Carenaje = x;
+        }
+
+        public string GetCarenaje()
+        {
+            return Carenaje;
+        }
+        public static string Pito;
+        public void SetPito(string x)
+        {
+            Pito = x;
+        }
+
+        public string GetPito()
+        {
+            return Pito;
+        }
+        public static string GBDel;
+        public void SetGBDel(string x)
+        {
+            GBDel = x;
+        }
+
+        public string GetGBDel()
+        {
+            return GBDel;
+        }
+        public static string FrenoDisco;
+        public void SetFrenoDisco(string x)
+        {
+            FrenoDisco = x;
+        }
+
+        public string GetFrenoDisco()
+        {
+            return FrenoDisco;
+        }
+        public static string Tablero;
+        public void SetTablero(string x)
+        {
+            Tablero = x;
+        }
+
+        public string GetTablero()
+        {
+            return Tablero;
+        }
+        public static string DirDelIzq;
+        public void SetDirDelIzq(string x)
+        {
+            DirDelIzq = x;
+        }
+
+        public string GetDirDelIz()
+        {
+            return DirDelIzq;
+        }
+        public static string EspIzq;
+        public void SetEspizq(string x)
+        {
+            EspIzq = x;
+        }
+
+        public string GetEspizq()
+        {
+            return EspIzq;
+        }
+        public static string ManDirIzq;
+        public void SetManDirIzq(string x)
+        {
+            ManDirIzq = x;
+        }
+
+        public string GetManDirIzq()
+        {
+            return ManDirIzq;
+        }
+        public static string ManIzq;
+        public void SetManIzq(string x)
+        {
+            ManIzq = x;
+        }
+
+        public string GetMainIzq()
+        {
+            return ManIzq;
+        }
+        public static string PrManIzq;
+        public void SetPrManIzq(string x)
+        {
+            PrManIzq = x;
+        }
+
+        public string GetPrManIzq()
+        {
+            return PrManIzq;
+        }
+        public static string DirDelDer;
+        public void SetDirDelDer(string x)
+        {
+            DirDelDer = x;
+        }
+
+        public string GetDirDelDer()
+        {
+            return DirDelDer;
+        }
+        public static string EspDer;
+        public void SetEspDer(string x)
+        {
+            EspDer = x;
+        }
+
+        public string GetEspDer()
+        {
+            return EspDer;
+        }
+        public static string ManDirDer;
+        public void SetManDirDer(string x)
+        {
+            ManDirDer = x;
+        }
+
+        public string GetManDirDer()
+        {
+            return ManDirDer;
+        }
+        public static string ManFreDer;
+        public void SetManFreDer(string x)
+        {
+            ManFreDer = x;
+        }
+
+        public string GetManFreDer()
+        {
+            return ManFreDer;
+        }
+        public static string PrManFreDer;
+        public void SetPrManFreDer(string x)
+        {
+            PrManFreDer = x;
+        }
+
+        public string GetPrManFreDer()
+        {
+            return PrManFreDer;
+        }
+        public static string TanCom;
+        public void SetTanCom(string x)
+        {
+            TanCom= x;
+        }
+
+        public string GetTanCom()
+        {
+            return TanCom;
+        }
+        public static string TTanCom;
+        public void SetTTanCom(string x)
+        {
+            TTanCom = x;
+        }
+
+        public string GetTTanCom()
+        {
+            return TTanCom;
+        }
+        public static string Barbero;
+        public void SetBarbero(string x)
+        {
+            Barbero = x;
+        }
+
+        public string GetBarbero()
+        {
+            return Barbero;
+        }
+        public static string Pata;
+        public void SetPata(string x)
+        {
+            Pata = x;
+        }
+
+        public string GetPata()
+        {
+            return Pata;
+        }
+        public static string Gato;
+        public void SetGato(string x)
+        {
+            Gato= x;
+        }
+
+        public string GetGato()
+        {
+            return Gato;
+        }
+        public static string TapLatIzq;
+        public void SetTapLatIzq(string x)
+        {
+            TapLatIzq = x;
+        }
+
+        public string GetTapLatIzq()
+        {
+            return TapLatIzq;
+        }
+        public static string GCad;
+        public void SetGCad(string x)
+        {
+            GCad = x;
+        }
+
+        public string GetGCad()
+        {
+            return GCad;
+        }
+        public static string Sillin;
+        public void SetSillin(string x)
+        {
+            Sillin = x;
+        }
+
+        public string GetSillin()
+        {
+            return Sillin;
+        }
+        public static string Parrilla;
+        public void SetParrilla(string x)
+        {
+            Parrilla = x;
+        }
+
+        public string GetParrilla()
+        {
+            return Parrilla;
+        }
+        public static string GBarDel;
+        public void SetGBarDel(string x)
+        {
+            GBarDel = x;
+        }
+
+        public string GetGBarDel()
+        {
+            return GBarDel;
+        }
+        public static string DirTraIzq;
+        public void SetDirTraIzq(string x)
+        {
+            DirTraIzq= x;
+        }
+
+        public string GetDirTraIzq()
+        {
+            return DirTraIzq;
+        }
+        public static string Amortiguadores;
+        public void SetAmortiguadores(string x)
+        {
+            Amortiguadores= x;
+        }
+
+        public string GetAmortiguadores()
+        {
+            return Amortiguadores;
+        }
+        public static string Exosto;
+        public void SetExosto(string x)
+        {
+            Exosto = x;
+        }
+
+        public string GetExosto()
+        {
+            return Exosto;
+        }
+        public static string Placa;
+        public void SetPlaca(string x)
+        {
+            Placa = x;
+        }
+
+        public string GetPlaca()
+        {
+            return Placa;
+        }
+        public static string DirTraDer;
+        public void SetDirTraDer(string x)
+        {
+            DirTraDer = x;
+        }
+
+        public string GetDirTraDer()
+        {
+            return DirTraDer;
+        }
+
+        public static string TapLatDer;
+        public void SetTapLatDer(string x)
+        {
+            TapLatDer = x;
+        }
+
+        public string GetTapLatDer()
+        {
+            return TapLatDer;
+        }
+        public static string Bateria;
+        public void SetBateriaa(string x)
+        {
+            Bateria = x;
+        }
+
+        public string GetBateria()
+        {
+            return Bateria;
+        }
+        public static string Tacos;
+        public void SetTacos(string x)
+        {
+            Tacos = x;
+        }
+
+        public string GetTacos()
+        {
+            return Tacos;
+        }
+        public static string PalFrePie;
+        public void SetPalFrePie(string x)
+        {
+            PalFrePie = x;
+        }
+
+        public string GetPalFrePuie()
+        {
+            return PalFrePie;
+        }
+        public static string Cran;
+        public void SetCran(string x)
+        {
+            Cran = x;
+        }
+
+        public string GetCran()
+        {
+            return Cran;
+        }
+        public static string Otros;
+        public void SetOtros(string x)
+        {
+            Otros = x;
+        }
+
+        public string GetOtros()
+        {
+            return Otros;
+        }
+        public static string Marca;
+        public void SetMarca(string x)
+        {
+            Marca = x;
+        }
+        public string GetMarca()
+        {
+            return Marca;
+        }
+
+        public static string PlacaNum;
+        public void SetPlacaNum(string x)
+        {
+            PlacaNum = x;
+        }
+        public string GetPlacaNum()
+        {
+            return PlacaNum;
+        }
+        public static string Color;
+        public void SetColor(string x)
+        {
+            Color = x;
+        }
+        public string GetColor()
+        {
+            return Color;
+        }
+        public static int debegrua;
+        public void Setdebegrua(int x)
+        {
+            debegrua = x;
+        }
+        public int Getdebegrua()
+        {
+            return debegrua;
+        }
+        public static int dejallaves;
+        public void Setdejallaves(int x)
+        {
+            dejallaves = x;
+        }
+        public int Getdejallaves()
+        {
+            return dejallaves;
+        }
 
     }
 }
